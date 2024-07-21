2511195cfc6cd73ee811b56fcf38f4ec97b0a212
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-21 13:19:28 +0300

    start add recipe api

--- api/addRecipe.php
+++ api/addRecipe.php
@@ -1,1 +1,20 @@
 <?php
+
+require '../confg.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $name = $_POST['name'];
+    $ingredients = $_POST['ingredients'];
+    $steps = $_POST['steps'];
+
+    $stmt = $conn->prepare('insert into recipes (name, ingridients, steps) values (?, ?, ?)');
+    $stmt->bind_param('sss', $name, $ingredients, $steps);
+    $stmt->execute();
+
+    echo json_encode(['status' => 'success', 'message' => 'recipe inserted']);
+
+
+} else {
+    echo json_encode(['error' => 'wrong request method']);
+}
