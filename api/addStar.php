c559546468290b6c69e4e0a57ff7fff9edb9cd4e
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-21 14:26:40 +0300

    finish addStar api

---
+++ api/addStar.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../confg.php';
+
+session_start();
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+
+    $user_id = $_SESSION['user_id'];
+    $recipe_id = $_GET['recipe_id'];
+
+    $stmt = $conn->prepare('insert into stars (user_id, recipe_id) values (?, ?)');
+    $stmt->bind_param('ii', $user_id, $recipe_id);
+    $stmt->execute();
+
+    echo json_encode(['message' => 'star added successfully']);
+
+}else{
+    echo json_encode(['error' => 'wrong method']);
+}
